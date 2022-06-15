be1bfc1abde9a9d0ce26bf0fb59c314416c0f40e
Jose Soto <jsoto7087@gmail.com>
2022-06-15 02:03:49 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+
+  for (let start = 0; start < array.length - 1; start++) {
+    for (let i = start + 1; i < array.length; i++) {
+      if (array[i] + array[start] === target) {
+        return true
+      }
+    }
+  }
+  return false;
 }
+
+
+
+
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,10 +21,20 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+
+  iterates over the array from front to end and end to front 
+
+
 */
 
 /*
-  Add written explanation of your solution here
+Get the first number from the array
+add all other numbers to that first number to see if the two number combination adds up to the targer.
+if yes return true.
+if not
+get the next number in the array and do the same thing.
+continew going through the array until a match is found. 
+if none return false.
 */
 
 // You can run `node index.js` to view these console logs
