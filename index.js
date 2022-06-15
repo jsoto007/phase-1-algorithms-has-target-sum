51b6df87261e0820aa433c6b95a95a24349856ab
Jose Soto <jsoto7087@gmail.com>
2022-06-14 21:39:11 -0400

    commit tree 2910764bd3b53ea10c1c38e62462b1e53f6abb1c

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+
+  for (let start = 0; start < array.length - 1; start++) {
+    for (let i = start + 1; i < array.length; i++) {
+      if (array[i] + array[start] === target) {
+        return true
+      }
+    }
+  }
+  return false;
 }
+
+
+
+
 
 /* 
   Write the Big O time complexity of your function here
@@ -8,10 +21,18 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+
+  iterates over the array from front to end and end to front 
+
+
 */
 
 /*
   Add written explanation of your solution here
+    iterates over the array
+  adds all numbers and conpares then to the target.
+  returns the number the numbers that add up to the target.
+  if No number add up to the target return none add up to the target.  
 */
 
 // You can run `node index.js` to view these console logs
